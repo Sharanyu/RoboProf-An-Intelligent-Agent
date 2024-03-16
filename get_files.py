29e36a1e56b1181bf1286613978b9d04c233f3bc
SushantSharma10 <33939375+SushantSharma10@users.noreply.github.com>
2024-03-16 19:50:23 -0400

    Categorize study material for courses.
    This script is tailored for managing educational content by organizing course materials into a structured and easily navigable format. It automates the process of sorting through a directory of files, categorizing them by course and material type,i.e. lecture slides, worksheets and lab material.

---
+++ get_files.py
@@ -0,0 +1,58 @@
+import os
+
+# List of courses with materials
+courses_with_materials = ["COMP474", "COMP6741", "SOEN6431"]
+
+course_materials = ["slides", "worksheets", "readings", "assignments", "course_outline"]
+folder_path = "data/course_materials/"
+
+
+# Function to find files within course-specific folders
+def find_files_in_course_folders(folder_path):
+    course_files = {}
+    for course in courses_with_materials:
+        course_files[course] = []
+
+    for root, dirs, files in os.walk(folder_path):
+        for course in courses_with_materials:
+            if f"/{course}" in root:
+                for file in files:
+                    full_path = os.path.join(root, file)
+                    abs_path = os.path.abspath(full_path)
+                    abs_path = abs_path.replace(" ", "%20")
+                    course_files[course].append(abs_path)
+
+    return course_files
+
+
+def collect(folder_path):
+    file_paths = []
+    course_files = find_files_in_course_folders(folder_path)
+    for course, files in course_files.items():
+        for file in files:
+            file_paths.append(file)
+
+    def should_exclude_path(path):
+        return ".DS_Store" in path or "Icon\r" in path
+
+    filtered_file_paths = [path for path in file_paths if not should_exclude_path(path)]
+    return filtered_file_paths
+
+
+# Function to categorize files for each course
+def categorize_files(courses_with_materials, course_materials):
+    file_paths = collect(folder_path)
+    categorized_files = {
+        course: {material: [] for material in course_materials}
+        for course in courses_with_materials
+    }
+
+    for path in file_paths:
+        for course in courses_with_materials:
+            if course in path:
+                for material in course_materials:
+                    if material in path:
+                        categorized_files[course][material].append(path)
+                        break
+                break
+    return categorized_files
